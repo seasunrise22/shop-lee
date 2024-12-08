927935686eceafb245de668658329bcf0b1845c7
lee <seasunrise22@gmail.com>
2024-12-08 15:22:39 +0900

    first commit

---
+++ src/main/java/com/lee/shop_lee/ShopLeeApplication.java
@@ -0,0 +1,13 @@
+package com.lee.shop_lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopLeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lee/shop_lee/ShopLeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.shop_lee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
